988dda5bd991bcacc861d70f3f315f7607d49cf3
juanpetterson <juan.petterson@gmail.com>
2020-06-08 21:39:36 -0300

    Start using types to prevent incorrect inputs

---
+++ app.ts
@@ -0,0 +1,14 @@
+function add(n1: number, n2: number): number {
+  // if (typeof n1 === "number" && typeof number2 === "number") {
+  //   throw new Error("Incorrect input!");
+  // } it`s just ajvascript validation
+
+  return n1 + n2;
+}
+
+const number1 = 5;
+const number2 = 2.8;
+
+const result = add(number1, number2);
+
+console.log(result);
